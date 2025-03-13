c899592e57f7a162c5b75419e21ca50e66fc8378
Crystal <99391710+x200706@users.noreply.github.com>
2025-03-13 15:12:36 +0800

    更新Blog Bio

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Crystal",
     image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
-    bio: "　",
+    bio: "忙與不忙的疊加態",
     email: "",
     linkedin: "crystalchen1997",
     github: "x200706",
